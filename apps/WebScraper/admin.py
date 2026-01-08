1af866dd85f0aa9784210d726d886fe004974a44
gilhooleyp <gilhooleyp@gmail.com>
2026-01-07 19:11:23 -0500

    fix: update admin to use correct PropertyListing fields

--- apps/WebScraper/admin.py
+++ apps/WebScraper/admin.py
@@ -1,23 +1,16 @@
 from django.contrib import admin
 from .models import PropertyListing
 
+
 class PropertyListingAdmin(admin.ModelAdmin):
-    # Fields to display in the admin list view
     list_display = (
-        'address', 'price', 'bedrooms', 'bathrooms', 'home_size', 'lot_size',
-        'property_type', 'garage', 'year_built', 'time_on_market'
+        'parcel_id', 'address', 'city', 'market_value', 'building_sqft',
+        'bedrooms', 'bathrooms', 'land_size', 'tax_status', 'last_scraped'
     )
-
-    # Fields to use as filter options
-    list_filter = ('property_type', 'bedrooms', 'bathrooms', 'stories', 'garage', 'year_built')
-
-    # Fields to add to the search functionality
-    search_fields = ('address', 'description', 'property_type')
-
-    # Settings for how many items per page
+    list_filter = ('city', 'property_type', 'tax_status', 'delinquent', 'bedrooms')
+    search_fields = ('parcel_id', 'address', 'owner_name', 'city')
     list_per_page = 25
+    readonly_fields = ('last_scraped', 'created_at')
 
-    # Settings to specify fields that should be editable in the list view
-    list_editable = ('price', 'time_on_market')
 
 admin.site.register(PropertyListing, PropertyListingAdmin)
