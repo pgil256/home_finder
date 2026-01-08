e26babab3d013b99d766da9bc3a55634a03aab70
gilhooleyp <gilhooleyp@gmail.com>
2026-01-07 19:09:25 -0500

    fix: remove duplicate SessionMiddleware

--- home_finder/settings.py
+++ home_finder/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
