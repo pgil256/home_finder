23ae119275ba8212c7df465cf1e34ddbdb21f022
gilhooleyp <gilhooleyp@gmail.com>
2026-01-09 20:44:21 -0500

    fix: add migrations to worker startup script

--- start-worker.sh
+++ start-worker.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "=== Starting Celery Worker ===" >&2
 
+echo "=== Running migrations ===" >&2
+python manage.py migrate
+
+echo "=== Starting Celery ===" >&2
 exec celery -A home_finder worker \
     --loglevel=info \
     --concurrency=2
